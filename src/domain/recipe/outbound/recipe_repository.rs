603a8d44d15761cc6bc18dc5fedc5c9d6993273c
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-03-08 19:37:14 +0800

    feat(domain): add recipe repository trait

---
+++ src/domain/recipe/outbound/recipe_repository.rs
@@ -0,0 +1,10 @@
+use anyhow::Result as AnyhowResult;
+
+use crate::domain::item::model::ItemId;
+use crate::domain::recipe::model::{Recipe, RecipeId};
+
+#[dynosaur::dynosaur(pub DynRecipeRepository = dyn(box) RecipeRepository)]
+pub trait RecipeRepository: Send + Sync {
+    async fn get(&self, recipe_id: &RecipeId) -> AnyhowResult<Option<Recipe>>;
+    async fn find_containing_product(&self, product_id: &ItemId) -> AnyhowResult<Vec<Recipe>>;
+}
