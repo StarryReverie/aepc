de5e39502dafa3d134aaa4a54e59c6e75d43f18c
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-03-27 23:03:23 +0800

    chore: use fileset to only include source codes when building

--- nix/package.nix
+++ nix/package.nix
@@ -7,7 +7,14 @@ rustPlatform.buildRustPackage {
   pname = "aepc";
   version = "0.1.0";
 
-  src = lib.cleanSource ../.;
+  src = lib.fileset.toSource {
+    root = ../.;
+    fileset = lib.fileset.unions [
+      ../Cargo.toml
+      ../Cargo.lock
+      ../src
+    ];
+  };
 
   cargoLock = {
     lockFile = ../Cargo.lock;
